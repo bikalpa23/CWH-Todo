a343ab5f7bc559d34ca5f1cb8f17f1c27bf95961
Bikalpa <bikalpasangat@gmail.com>
2024-01-08 21:43:23 +0545

    Tdos

---
+++ CWH/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Header from './Components/header'
+import Todos from './Components/Todos'
+import Footer from './Components/Footer'
+function App() {
+
+  return (
+    <>
+    <Header title = "My Todos List"/>
+    <Todos/>
+    <Footer/>
+    </>
+  )
+}
+
+export default App
